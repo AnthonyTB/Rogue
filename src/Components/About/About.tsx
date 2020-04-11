96fc991c2ea51a46c47d053a09c0f9fb39843773
AnthonyTB <anthonylol@protonmail.com>
2020-04-11 00:43:17 -0700

    completed counter incrementer feature

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,21 +1,92 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useReducer, useCallback } from 'react';
 import './About.css';
 import { Context } from '../Context/Context';
+import { Reducer } from '../interfaces/interfaces';
 
 function About() {
   const { twitterData, twitchData, youtubeSubscribers } = useContext(Context);
 
-  const incrementVals = (val: number) => {
-    let finalVal = 0;
-    const interval = setInterval(() => {
-      if (finalVal < val) {
-        finalVal++;
-      } else {
-        clearInterval(interval);
-        return finalVal;
-      }
-    }, 1000);
+  const Reducer = (prevState: any, { type, payload }: Reducer) => {
+    switch (type) {
+      case 'twitterCount':
+        return {
+          ...prevState,
+          twitterCount: payload.twitterCount,
+        };
+      case 'twitchCount':
+        return {
+          ...prevState,
+          twitchCount: payload.twitchCount,
+        };
+      case 'youtubeCount':
+        return {
+          ...prevState,
+          youtubeCount: payload.youtubeCount,
+        };
+      default:
+        return '';
+    }
   };
+
+  const [state, dispatch] = useReducer(Reducer, {
+    twitterCount: 0,
+    twitchCount: 0,
+    youtubeCount: 0,
+  });
+
+  const dataSetter = (section: string, data: number) => {
+    dispatch({
+      type: `${section}`,
+      payload: {
+        [section]: data,
+      },
+    });
+  };
+
+  const incrementVals = useCallback(
+    (source: string, sourceVal: number, maxVal: number): any => {
+      const interval = setInterval(() => {
+        if (sourceVal < maxVal) {
+          sourceVal++;
+          dataSetter(source, sourceVal);
+        } else {
+          clearInterval(interval);
+          return sourceVal;
+        }
+      }, 0.0);
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (state.twitterCount === 0 && state.twitchCount === 0) {
+      incrementVals(
+        'twitterCount',
+        state.twitterCount,
+        twitterData.followers_count
+      );
+
+      incrementVals(
+        'twitchCount',
+        state.twitchCount,
+        twitchData.data[0].view_count
+      );
+
+      incrementVals(
+        'youtubeCount',
+        state.youtubeCount,
+        youtubeSubscribers.statistics.subscriberCount
+      );
+    }
+  }, [
+    incrementVals,
+    state.twitchCount,
+    state.twitterCount,
+    state.youtubeCount,
+    twitchData.data,
+    twitterData.followers_count,
+    youtubeSubscribers.statistics.subscriberCount,
+  ]);
 
   return (
     <>
@@ -23,19 +94,15 @@ function About() {
         <ul>
           <li>
             <h5>@{twitterData.screen_name}</h5>
-            <span className='Follower-Count'>
-              {incrementVals(twitterData.followers_count)}
-            </span>
+            <span className='Follower-Count'>{state.twitterCount}</span>
           </li>
           <li>
             <h5>/{twitchData.data[0].display_name}</h5>
-            <span className='Views-Count'>{twitchData.data[0].view_count}</span>
+            <span className='Views-Count'>{state.twitchCount}</span>
           </li>
           <li>
             <h5>/Rogue</h5>
-            <span className='Subscriber-Count'>
-              {youtubeSubscribers.statistics.subscriberCount}
-            </span>
+            <span className='Subscriber-Count'>{state.youtubeCount}</span>
           </li>
         </ul>
       </div>
--- src/Components/interfaces/interfaces.ts
+++ src/Components/interfaces/interfaces.ts
@@ -1,3 +1,8 @@
 export interface ResponseObject {
   response: object;
 }
+
+export interface Reducer {
+  type: string;
+  payload: any;
+}
